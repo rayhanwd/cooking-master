fd51c3da94a3e547e5ab183b2f1edbca918344a2
Kazi Rayhan <rayhanbd4400@gmail.com>
2021-04-05 18:14:31 +0600

    updated

--- app.js
+++ app.js
@@ -1,58 +1,64 @@
-document.getElementById('search_button').addEventListener('click',function(e){
+
+let input = document.getElementById('search-name').value;
+
+
+document.getElementById('search_button').addEventListener('click', function (e) {
     e.preventDefault();
-
-let  input = document.getElementById('search-name').value;   
-    
-fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
-
-.then(response =>response.json())
-.then(data =>{
-let html = "";
-if(data.meals){
-    data.meals.forEach(meal =>{
-        html += `
-        <div onclick="expandMore()" id="single_item" class="single-item">
-        <img class="Thumbnail-img" src="${meal.strMealThumb}" alt="">
-        <h4 class="meal-name">${meal.strMeal}</h4>
-        <div id="more-info">
-        <h4>Ingredients</h4>
-        <ul>
-        <li><img src="checkbox-fill.png" alt=""> ${meal.strIngredient1}</li>
-        <li><img src="checkbox-fill.png" alt=""> ${meal.strIngredient2}</li>
-        <li><img src="checkbox-fill.png" alt=""> ${meal.strIngredient3}</li>
-        <li><img src="checkbox-fill.png" alt=""> ${meal.strIngredient4}</li>
-        <li><img src="checkbox-fill.png" alt=""> ${meal.strIngredient5}</li>
-        <li><img src="checkbox-fill.png" alt=""> ${meal.strIngredient6}</li>
-        </ul>
-        </div>
-        </div>
-        `;
-    });
-}
-
-else{
-    html += `
-    <div class="error-message">
-    <h3>Search No Result
-    </h3>
-    <p>We're sorry. We cannot find any matches for your search term.</p>
-    </div>
-    `;
-}
-
-document.getElementById('meal-info').innerHTML = html;
-
-
+    showMeal()
 });
 
-})
-function expandMore(){
-const moreDetails =document.getElementById('more-info');
-for (let i = 0; i < moreDetails.length; i++) {
-    const moreDetail = moreDetails[i];   
+// photo  strMealThumb
+//name  strMeal
+
+const showMeal = () => {
+    const parentDiv = document.getElementById('food-info');
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
+
+        .then(res => res.json())
+        .then(data => {
+            let item = data.meals;
+            //console.log(item.length);
+            for (let i = 0; i < item.length; i++) {
+                const food = item[i];
+                //console.log(food);
+                const foodInfoDiv = document.createElement('div');
+                foodInfoDiv.className = 'food-item';
+                const foodInfo = `
+             <img src="${food.strMealThumb}" />
+             <h3 onclick="showDetails('${food.idMeal}')" class="food-text">${food.strMeal}</h3>
+             `
+                foodInfoDiv.innerHTML = foodInfo;
+                parentDiv.appendChild(foodInfoDiv);
+            };
+        });
+}
+
+const showDetails = (id) => {
+    const ChildDiv = document.getElementById('food-details');
+    ChildDiv.innerHTML = ' ';
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}
+    `)
+        .then(res => res.json())
+        .then(foodDetails => {
+            const foodDetail = foodDetails.meals;
+            const div = document.createElement('div');
+            div.className = 'food';
+            foodDetail.forEach(element => {
+                const h4 = document.createElement('h4');
+                h4.innerHTML = element.strMeal;
+                const p = document.createElement('p');
+                p.innerHTML = element.strInstructions;
+                div.appendChild(h4);
+                div.appendChild(p);
+                ChildDiv.appendChild(div);
+                div.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    div.style.display = 'none';
+                })
+            })
+        })
 
 }
 
-moreDetails.style.display ="block";
 
-}
